aace0ab052344fdf161ceafa3b74fe877a27f52f
Aryan Gupta <aryan@Aryans-MacBook-Air.local>
2020-07-01 14:25:15 +0530

    Updates

---
+++ precache-manifest.df3ea868b86df3b1629ddbf4c41aec1e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34945483362185db187fa932c51a9c8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6b64080cbc1feae3128",
+    "url": "/static/css/main.1874118c.chunk.css"
+  },
+  {
+    "revision": "93b980c1db8e093d0dca",
+    "url": "/static/js/2.0b6d758d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0b6d758d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6b64080cbc1feae3128",
+    "url": "/static/js/main.e7b32743.chunk.js"
+  },
+  {
+    "revision": "c5a17faaefd1181082a5",
+    "url": "/static/js/runtime-main.d46950a8.js"
+  },
+  {
+    "revision": "fde3c0b1ed24a390fd6180981c7439c8",
+    "url": "/static/media/award.fde3c0b1.jpg"
+  },
+  {
+    "revision": "305b7b7bf17b8341d4c61d0dc017fb9d",
+    "url": "/static/media/cloud.305b7b7b.svg"
+  },
+  {
+    "revision": "f9c8b12757db632cbd634271b1cd5b10",
+    "url": "/static/media/data_analytics_.f9c8b127.svg"
+  },
+  {
+    "revision": "8c7ae19756b45657b73055a1142be613",
+    "url": "/static/media/data_process.8c7ae197.svg"
+  },
+  {
+    "revision": "73e3ab59751b8dd4bf5c6c8be3335e21",
+    "url": "/static/media/language.73e3ab59.png"
+  },
+  {
+    "revision": "54c3adc538f06c7819b16b934f876911",
+    "url": "/static/media/mountain.54c3adc5.svg"
+  },
+  {
+    "revision": "921df655e6f1ce400074caecfdf30605",
+    "url": "/static/media/process_building.921df655.svg"
+  },
+  {
+    "revision": "9e5663bc5a0db818f2eaf47a2567f092",
+    "url": "/static/media/publish.9e5663bc.jpg"
+  },
+  {
+    "revision": "756b31836ca3dfc446be2b22b4962be9",
+    "url": "/static/media/skill.756b3183.jpg"
+  },
+  {
+    "revision": "fafe0d09bc8d6a2b0848c96d52b68f36",
+    "url": "/static/media/sun.fafe0d09.svg"
+  }
+]);
